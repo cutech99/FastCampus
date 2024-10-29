fd3c7045d118303c426daff1827549e34b30b9e0
JaeWon <karenhite9803@gmail.com>
2024-10-30 01:30:30 +0900

    fast campus src file

---
+++ src/fc/java/model/Dog.java
@@ -0,0 +1,7 @@
+package fc.java.model;
+
+public class Dog {
+    public void eat(){
+        System.out.println("개처럼 먹다");
+    }
+}
---
+++ src/fc/java/part4/EmployeeInitTest.java
@@ -0,0 +1,8 @@
+package fc.java.part4;
+
+public class EmployeeInitTest {
+    public static void main(String[] args) {
+        RempVO vo=new RempVO("손명준",26,"010-8112-4545","2022-10-10","홍보부",false);
+        System.out.println(vo.toString());
+    }
+}
